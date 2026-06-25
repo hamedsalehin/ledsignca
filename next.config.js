e84d10b532ee2a769c3538cb6f3219f883e5c7ff
hamedsalehin <nanosign1@gmail.com>
2026-06-24 23:42:36 -0400

    fix: remove infinite redirect loop for /projects

--- next.config.js
+++ next.config.js
@@ -88,8 +88,6 @@ const nextConfig = {
       { source: "/contact-rgb-sign-screen-sign-for-store-citylight-group", destination: "/contact-us", permanent: true },
       { source: "/refund_returns/", destination: "/return-policy", permanent: true },
       { source: "/refund_returns", destination: "/return-policy", permanent: true },
-      { source: "/projects/", destination: "/projects", permanent: true },
-      { source: "/projects", destination: "/projects", permanent: true },
       { source: "/support/", destination: "/contact-us", permanent: true },
       { source: "/support", destination: "/contact-us", permanent: true },
       { source: "/request-quote/", destination: "/get-a-quote", permanent: true },
