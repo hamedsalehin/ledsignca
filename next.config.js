15e5049df2f902e6dd51eb87cce6981d6f39601d
hamedsalehin <nanosign1@gmail.com>
2026-06-26 02:25:56 -0400

    Hotfix: Restore unoptimized images and domains in next.config.js to fix broken images on AWS Amplify

--- next.config.js
+++ next.config.js
@@ -15,6 +15,7 @@ const nextConfig = {
   },
   allowedDevOrigins: ["*.preview.same-app.com"],
   images: {
+    unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
