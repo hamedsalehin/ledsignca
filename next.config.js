45fdd45b5b1a3d94597707e52ca59fa391d8f0fc
Hamed Salehin <hamedsalehin@gmail.com>
2026-08-11 11:33:27 -0400

    fix(cache): set HTML Cache-Control to must-revalidate to prevent stale CDN edge caching

--- next.config.js
+++ next.config.js
@@ -403,7 +403,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=0, s-maxage=3600, stale-while-revalidate=86400",
+            value: "public, max-age=0, must-revalidate",
           },
         ],
       },
